0f9afc98397873ffbe2f2cece17c777d9afc163d
student <uivankurchenko@Gmail.com>
2020-09-25 19:02:29 +0300

    Добавьте файлы проекта.

---
+++ proek4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+  static void Main(string[] args)
+  {
+    ArrayList myList = new ArrayList
+    {
+      "First",
+      "Second",
+      "Third",
+      "Fourth"
+    }; foreach (string item in myList)
+    {
+      Console.WriteLine("Unsorted: {0}", item);
+    }
+    // Сортировка при помощи стандартного объекта сравнения 
+    myList.Sort();
+    foreach (string item in myList)
+    {
+      Console.WriteLine("   Sorted: {0}", item);
+    }
+  }
+}
