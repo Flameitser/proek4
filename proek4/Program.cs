ae44e0407330cc026cbe41164b095b7b1126ff25
student <uivankurchenko@Gmail.com>
2020-09-25 19:17:40 +0300

    1

--- proek4/Program.cs
+++ proek4/Program.cs
@@ -3,26 +3,18 @@ class Program
 {
   static void Main(string[] args)
   {
-    Hashtable lookup = new Hashtable();
-
-    lookup["0"] = "Zero";
-    lookup["1"] = "One";
-    lookup["2"] = "Two";
-    lookup["3"] = "Three";
-    lookup["4"] = "Four";
-    lookup["5"] = "Five";
-    lookup["6"] = "Six";
-    lookup["7"] = "Seven";
-    lookup["8"] = "Eight";
-    lookup["9"] = "Nine";
-
-    string ourNumber = 888 - 555 - 1212";
-  foreach (char с in ourNumber)
+    ArrayList myList = new ArrayListO; myList.Add("First"); myList.Add("Second");
+    myList.Add("Third");
+    myList.Add("Fourth");
+    foreach (string item in myList)
     {
-      string digit = c.ToString(); if (lookup.ContainsKey(digit))
-      {
-        Console.WriteLine(lookup[digit]);
-      }
+      Console.WriteLine("Unsorted: {0}", item);
+    }
+    // Сортировка при помощи стандартного объекта сравнения 
+    myList.Sort();
+    foreach (string item in myList)
+    {
+      Console.WriteLine("   Sorted: {0}", item);
     }
   }
 }
